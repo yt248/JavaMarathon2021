11f3ccc0fc50a2208c68f7c7f9c72946db607854
Yauhen <yauhen.tsaryk@gmail.com>
2021-03-27 08:53:12 +0300

    add day 7

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,43 @@
+package day7;
+
+public class Player {
+
+    public static int MAX_STAMINA = 100; // “максимальная выносливость”
+    public static int MIN_STAMINA = 0;   // “минимальная выносливость”
+
+    private int stamina;                  //“выносливость”
+
+    public static int countPlayers;
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+        if (countPlayers < 6)
+            countPlayers++;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+        if (stamina == 0) {
+            System.out.println("Игроку нужен отдых, он уходит");
+            countPlayers--;
+        }
+        if (stamina > 0) {
+            stamina--;
+        }
+    }
+
+    static public void info() {
+        if (countPlayers <= 6) {
+            System.out.println("Команды неполные. На поле еще есть " + (6 - countPlayers) + " свободных мест");
+        } else {
+            System.out.println("На поле нет свободных мест");
+        }
+    }
+}
--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -1,7 +1,19 @@
 package day7;
 
+
+/*
+1. Для этого задания скопируйте класс Самолет из предыдущего дня в пакет текущего дня.
+В классе Самолет реализовать статический метод compareAirplanes,
+ который в качестве аргументов принимает два объекта класса Airplane (два самолета) и выводит сообщение в консоль о том, какой из самолетов длиннее.
+
+ */
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane airplane1 = new Airplane("British", 2015, 40, 15);
+        Airplane airplane2 = new Airplane("British", 2015, 60, 15);
+        Airplane airplane3 = new Airplane("British", 2015, 60, 15);
+        Airplane.compareAirplanes(airplane1, airplane2);
+        System.out.println();
+        Airplane.compareAirplanes(airplane2, airplane3);
     }
 }
