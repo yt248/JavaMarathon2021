580955976387d5b101e6797fe2debbd483b28bec
Yauhen <yauhen.tsaryk@gmail.com>
2021-04-04 09:48:25 +0300

    add day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,46 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String path = "src/main/resources/shoes.csv";
+        String missing_shoes = "src/main/resources/missing_shoes.txt";
+
+
+        File file = new File(path);
+        String[] newSplitLine = new String[0];
+        try {
+            PrintWriter pw = new PrintWriter(missing_shoes);
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] splitLine = line.split(";");
+                for (int i = 0; i < splitLine.length; i++) {
+                    if (Integer.parseInt(splitLine[i += 2]) == 0) {
+                        for (int j = i - 2; j < 3; j++) {
+                            if (j < 2) {
+                                pw.print(splitLine[j]);
+                                pw.print(", ");
+                            } else {
+                                pw.print(splitLine[j]);
+                                pw.println();
+                            }
+                        }
+                    }
+                }
+            }
+
+            pw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
+
