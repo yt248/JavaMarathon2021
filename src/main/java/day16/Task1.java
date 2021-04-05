558c9425ce7988586282f3f3f4b050b112254315
Yauhen <yauhen.tsaryk@gmail.com>
2021-04-05 12:44:58 +0300

    add day 16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,47 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+1. Реализовать программу, которая на вход принимает txt файл с целыми числами (можно использовать файл из задания 1 дня 14) и
+ в качестве ответа выводит в консоль среднее арифметическое этих чисел.
+Ответ будет являться вещественным числом. В консоль необходимо вывести полную запись вещественного числа
+(со всеми знаками после запятой) и сокращенную запись (с 3 знаками после запятой).
+
+Детали реализации: В классе Task1 создать публичный статический метод printResult(File file),
+в котором реализовать вышеописанную логику. В методе main() класса Task1 вызвать метод printResult(File file),
+передав ему в качестве аргумента объект класса File (txt файл с целыми числами).
+
+Пример:
+Числа в txt файле: 5 2 8 34 1 36 77
+Ответ: 23.285714285714285 --> 23.286
+
+ */
 public class Task1 {
     public static void main(String[] args) {
+        String path = "task1Day14.txt";
+        File file = new File(path);
+        printResult(file);
+    }
 
+    public static void printResult(File file) {
+        try {
+            double allSum = 0;
+            int counter = 0;
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                allSum += Integer.parseInt(scanner.nextLine());
+                counter++;
+            }
+
+            String formattedDouble = String.format("%.3f", allSum / counter);
+            System.out.println("Ответ: " + allSum / counter + " --> " + formattedDouble);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,61 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+2. Реализовать программу, записывающую числа разных типов в 2 файла.
+Файл 1 должен называться “file1.txt”, а Файл 2 должен называться “file2.txt”.
+Оба файла должны находиться в корне проекта. Создаваться файлы должны не вручную, а при запуске программы.
+
+Файл 1 должен содержать 1000 случайных чисел от 0 до 100.
+Файл 2 создается на основании Файла 1, но содержит числа вещественного типа данных.
+Метод заполнения Файла 2 следующий: для каждой группы из 20 целых чисел из Файла 1 рассчитывается их среднее арифметическое.
+ Затем, полученное значение записывается в Файл 2. Таким образом в Файле 2 будет находиться 50 вещественных чисел (1000 / 20 = 50).
+После того, как Файл 2 будет сформирован, необходимо реализовать статический метод printResult(File file).
+Этот метод должен рассчитать сумму всех вещественных чисел из Файла 2 и вывести её в консоль, отбросив вещественную часть.
+
+ */
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String pathFile1 = "file1.txt";
+        String pathFile2 = "file2.txt";
+        File file1 = new File(pathFile1);
+        File file2 = new File(pathFile2);
+
+        PrintWriter pw1 = new PrintWriter(pathFile1);
+        PrintWriter pw2 = new PrintWriter(pathFile2);
+        Random random = new Random();
+        double counter;
+
+        for (int i = 0; i < 1000; i++)
+            pw1.println(random.nextInt(100));
+
+        pw1.close();
+        Scanner scanner = new Scanner(file1);
+        while (scanner.hasNextLine()) {
+            counter = 0;
+            for (int i = 0; i < 20; i++) {
+                counter += Double.parseDouble(scanner.nextLine());
+            }
+            pw2.println(counter / 20);
+        }
+        pw2.close();
+
+        printResult(file2);
+    }
+
+    public static void printResult(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        double sumAll = 0;
+        while (scanner.hasNextLine()) {
+            sumAll += Double.parseDouble(scanner.nextLine());
+        }
+        System.out.println(sumAll);
+        System.out.println((int) sumAll);
 
     }
 }
